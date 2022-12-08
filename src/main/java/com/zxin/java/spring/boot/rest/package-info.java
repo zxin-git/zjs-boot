262806c4f8aff1fed71f755ae42c261c25933711
张鑫 <zhangxin@axzo.cn>
2022-12-08 16:50:44 +0800

    package-info.java

---
+++ src/main/java/com/zxin/java/spring/boot/rest/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *
+ * https://docs.spring.io/spring-framework/docs/5.1.8.RELEASE/spring-framework-reference/integration.html#rest-client-access
+ * @author zxin
+ */
+package com.zxin.java.spring.boot.rest;
